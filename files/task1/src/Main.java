c5d70fd2ae672a091d84c0d2e25b61eb0de81b3a
KoshelevVA <koshelevva96@gmail.com>
2025-03-23 21:32:37 +0400

    fix files.task1

--- files/task1/src/Main.java
+++ files/task1/src/Main.java
@@ -8,36 +8,43 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         final String PATH = "D:\\Projects\\jd_homeworks\\files\\task1\\Games";
+        boolean resultCreate = true;
         StringBuilder log = new StringBuilder();
-        List<String> dirs1 = List.of("src", "res", "savegames", "temp");
+        new File(PATH, "temp").mkdirs();
+
+        List<String> dirs1 = List.of("src", "res", "savegames");
         List<String> dirs2 = List.of("main", "test");
         List<String> dirs3 = List.of("drawables", "vectors", "icons");
         List<String> files1 = List.of("Main.java", "Utils.java");
-        if (createDir(new File(PATH), dirs1, log) &&
-                createDir(new File(PATH + "\\src"), dirs2, log) &&
-                createFile(new File(PATH+"\\src\\main"), files1, log) &&
-                createDir(new File(PATH + "\\res"), dirs3, log)) {
+
+        resultCreate &= createDir(new File(PATH), dirs1, PATH);
+        resultCreate &= createDir(new File(PATH + "\\src"), dirs2, PATH);
+        resultCreate &= createFile(new File(PATH + "\\src\\main"), files1, PATH);
+        resultCreate &= createDir(new File(PATH + "\\res"), dirs3, PATH);
+
+        if (resultCreate) {
             log.append(LocalTime.now() + " [INFO] " + "Папки и файлы успешно созданы");
         } else {
             log.append(LocalTime.now() + " [ERROR] " + "Папки и файлы не созданы");
         }
-        try (BufferedWriter bw = new BufferedWriter (new FileWriter(new File(PATH + "\\temp", "temp.txt")))) {
-            bw.write(log.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+        writeLog(PATH, "temp.txt", log.toString());
     }
 
-    public static boolean createDir(File parentDir, List<String> dirs, StringBuilder log) {
+    public static boolean createDir(File parentDir, List<String> dirs, String pathToLog) {
         boolean result = true;
+        StringBuilder log = new StringBuilder();
         for (String dir : dirs) {
             if (new File(parentDir, dir).exists()) {
-                log.append(LocalTime.now() + " [ERROR] " + "Директория \"" + dir + "\" уже существует по пути: " + parentDir.getAbsolutePath() + "\n");
+                writeLog(pathToLog, "temp.txt", LocalTime.now()
+                        + " [ERROR] " + "Директория \"" + dir + "\" уже существует по пути: " + parentDir.getAbsolutePath() + "\n");
             } else {
                 if (new File(parentDir, dir).mkdirs()) {
-                    log.append(LocalTime.now() + " [INFO] " + "Директория \"" + dir + "\" создана по пути: " + parentDir.getAbsolutePath() + "\n");
+                    writeLog(pathToLog, "temp.txt", LocalTime.now()
+                            + " [INFO] " + "Директория \"" + dir + "\" создана по пути: " + parentDir.getAbsolutePath() + "\n");
                 } else {
-                    log.append(LocalTime.now() + " [ERROR] " + "Директория \"" + dir + "\" не создана по пути: " + parentDir.getAbsolutePath() + "\n");
+                    writeLog(pathToLog, "temp.txt", LocalTime.now()
+                            + " [ERROR] " + "Директория \"" + dir + "\" не создана по пути: " + parentDir.getAbsolutePath() + "\n");
                     result = false;
                 }
             }
@@ -45,17 +52,21 @@ public class Main {
         return result;
     }
 
-    public static boolean createFile(File parentDir, List<String> files, StringBuilder log) {
+    public static boolean createFile(File parentDir, List<String> files, String pathToLog) {
         boolean result = true;
+        StringBuilder log = new StringBuilder();
         for (String file : files) {
             try {
                 if (new File(parentDir, file).exists()) {
-                    log.append(LocalTime.now() + " [ERROR] " + "Файл \"" + file + "\" уже существует по пути: " + parentDir.getAbsolutePath() + "\n");
+                    writeLog(pathToLog, "temp.txt", LocalTime.now()
+                            + " [ERROR] " + "Файл \"" + file + "\" уже существует по пути: " + parentDir.getAbsolutePath() + "\n");
                 } else {
                     if (new File(parentDir, file).createNewFile()) {
-                        log.append(LocalTime.now() + " [INFO] " + "Файл \"" + file + "\" создан по пути: " + parentDir.getAbsolutePath() + "\n");
+                        writeLog(pathToLog, "temp.txt", LocalTime.now()
+                                + " [INFO] " + "Файл \"" + file + "\" создан по пути: " + parentDir.getAbsolutePath() + "\n");
                     } else {
-                        log.append(LocalTime.now() + " [ERROR] " + "Файл \"" + file + "\" не создан по пути: " + parentDir.getAbsolutePath() + "\n");
+                        writeLog(pathToLog, "temp.txt", LocalTime.now()
+                                + " [ERROR] " + "Файл \"" + file + "\" не создан по пути: " + parentDir.getAbsolutePath() + "\n");
                         result = false;
                     }
                 }
@@ -64,5 +75,13 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public static void writeLog(String path, String filename, String logs) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path + "\\temp", filename), true))) {
+            bw.write(logs);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
