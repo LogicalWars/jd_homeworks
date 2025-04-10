ac367cec86be4d3e10ed39c9841f7807ea044de0
KoshelevVA <koshelevva96@gmail.com>
2025-04-10 20:14:38 +0400

    init sync task2

--- synchronization/sync.task1/src/Main.java
+++ synchronization/sync.task1/src/Main.java
@@ -12,11 +12,30 @@ public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Future<?>> futures = new ArrayList<>();
+        Thread thread = new Thread(() -> {
+            synchronized (sizeToFreq) {
+                while (!Thread.interrupted()) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    sizeToFreq.entrySet().stream()
+                            .max(Map.Entry.comparingByValue())
+                            .ifPresent(entry -> System.out.printf(
+                                            "Лидер повторений: %d (встретилось %d раз)%n",
+                                            entry.getKey(), entry.getValue()));
+                }
+            }
+        });
+        thread.start();
+
         for (int i = 0; i < countThreads; i++) {
             Future<?> future = executer.submit(() -> {
                 synchronized (sizeToFreq) {
                     int result = calculateRepeatOfR(generateRoute("RLRFR", 100), 'R');
                     sizeToFreq.put(result, sizeToFreq.getOrDefault(result, 0) + 1);
+                    sizeToFreq.notify();
                 }
             });
             futures.add(future);
@@ -24,17 +43,21 @@ public class Main {
         for (Future<?> future : futures) {
             future.get();
         }
+        thread.interrupt();
+        thread.join();
         executer.shutdown();
 
         List<Map.Entry<Integer, Integer>> list = sizeToFreq.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
                 .toList();
 
+        System.out.println("---");
         System.out.printf("Самое частое количество повторений %d (встретилось %d раз)%n", list.getFirst().getKey(), list.getFirst().getValue());
         System.out.println("Другие размеры: ");
         for (int i = 1; i < list.size(); i++) {
             System.out.printf("- %d (%d раз)%n", list.get(i).getKey(), list.get(i).getValue());
         }
+
     }
 
     public static int calculateRepeatOfR(String string, char ch) {
